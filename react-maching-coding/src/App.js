de8f470562fbd7c1471a3b7470ec8a7d48b4c569
Ranuj Chaudhary <ranujchoudhary@gmail.com>
2024-12-18 21:45:11 +0530

    progress bar

--- react-maching-coding/src/App.js
+++ react-maching-coding/src/App.js
@@ -1,7 +1,24 @@
+import { useEffect, useState } from "react";
 import "./App.css";
-
+import ProgreessBar from "./components/ProgreessBar";
 function App() {
-  return <div className="App"></div>;
+  const [value, setValue] = useState(0);
+  const [success, setSuccess] = useState(false);
+  useEffect(() => {
+    const id = setInterval(() => {
+      setValue((value) => value + 10);
+    }, 200);
+    return () => {
+      clearInterval(id);
+    };
+  }, []);
+
+  return (
+    <div className="App">
+      <ProgreessBar value={value} onComplete={() => setSuccess(true)} />
+      {<span>{!success ? "Loading..." : "Completed"}</span>}
+    </div>
+  );
 }
 
 export default App;
