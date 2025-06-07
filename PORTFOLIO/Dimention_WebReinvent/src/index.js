874da5167f5042a4f873780edf6b3d81625cea45
Ranuj Chaudhary <ranujchoudhary@gmail.com>
2025-06-07 23:00:39 +0530

    chat grid panel chat convertation

--- PORTFOLIO/Dimention_WebReinvent/src/index.js
+++ PORTFOLIO/Dimention_WebReinvent/src/index.js
@@ -1,24 +1,3 @@
 import "./style.css";
 
-// // Horizontal and Vertical Star Blinking
-// const horizontalStar = document.querySelector(".horizontal__star__blinking");
-// const verticalStar = document.querySelector(".vertical__star__blinking");
-
-// const horizontalStars = ['images/star-pattern-1.svg', 'images/star-pattern-2.svg', 'images/star-pattern-3.svg', 'images/star-pattern-4.svg']
-// // const verticalStars = horizontalStars
-// const verticalStars = ['images/vertical-star-1.svg', 'images/vertical-star-2.svg', 'images/vertical-star-3.svg', 'images/vertical-star-4.svg']
-
-// let i = -1;
-// let j = -1;
-
-//  setInterval(()=> {
-//     i++  
-//     j++
-//     if(i === horizontalStars.length) i = 0;
-//     if(j === verticalStars.length) j = 0;
-
-//    horizontalStar.src = horizontalStars[i];
-//    verticalStar.src = verticalStars[i];
- 
-// }, 2000)
 
